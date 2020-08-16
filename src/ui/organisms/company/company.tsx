4c9053dd685a3197e409e2dd6ce5280567010df1
DELINSKI ROMAN <Wendigo91@inbox.ru>
2020-08-17 01:00:48 +0300

    console clear

--- src/ui/organisms/company/company.tsx
+++ src/ui/organisms/company/company.tsx
@@ -134,9 +134,6 @@ export const Company = () => {
     setGetRout(slug);
     setGetValueName('');
   };
-  console.log(googleMap);
-  console.log(profile);
-  console.log(getRout);
 
   return (
     <>
